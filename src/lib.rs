51b9ae61721bb45470a80fe580d5c910e19eb946
Clark Kampfe <clark.kampfe@gmail.com>
2019-02-19 22:29:30 -0600

    add memory unsafety warning

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,16 @@ pub struct Atom<T: Clone> {
 unsafe impl<T: Clone> Sync for Atom<T> {}
 unsafe impl<T: Clone> Send for Atom<T> {}
 
+/*
+WARNING:
+This code is not safe.
+
+All of the code that reads/writes raw pointers has
+memory leaks. See: https://aturon.github.io/blog/2015/08/27/epoch/
+
+You should not use this code. It is for exploration/demonstration.
+*/
+
 impl<T: Clone> Atom<T> {
     pub fn new(value: T) -> Atom<T> {
         let boxed = Box::new(value);
@@ -25,17 +35,15 @@ impl<T: Clone> Atom<T> {
 
     pub fn reset(&self, new_value: T) -> T {
         let mut mutable_clone = new_value.clone();
-        &self
-            .atomic_pointer
+        self.atomic_pointer
             .store(&mut mutable_clone, Ordering::SeqCst);
         new_value
     }
 
     pub fn swap(&self, f: &Fn(&T) -> T) -> T {
-        unsafe {
             loop {
                 let current = &self.atomic_pointer.load(Ordering::SeqCst);
-                let fn_application_result = Box::into_raw(Box::new(f(&**current)));
+                let fn_application_result = unsafe { Box::into_raw(Box::new(f(&**current))) };
                 let old = &self.atomic_pointer.compare_and_swap(
                     *current,
                     fn_application_result,
@@ -44,18 +52,16 @@ impl<T: Clone> Atom<T> {
 
                 // if old == current, it means value was updated
                 if old == current {
-                    return (*fn_application_result).clone();
+                    unsafe { return (*fn_application_result).clone(); }
                 }
             }
-        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Atom;
-    use rand;
-    use rand::Rng;
+    use std::sync::Arc;
     use std::thread;
 
     #[test]
@@ -77,84 +83,82 @@ mod tests {
     }
 
     #[test]
-    fn it_works() {
-        use std::sync::Arc;
-
+    fn it_swaps_with_cheap_work() {
         let mut thread_handles = vec![];
-        let mut i_total = 0;
-
-        let inner = 0usize;
-        let atom = Arc::new(Atom::new(inner));
         let m = vec![];
         let results_vec: Arc<Atom<Vec<usize>>> = Arc::new(Atom::new(m));
 
-        let n = 50;
+        let n = 500;
 
         for _ in 0..n {
-            let arc = Arc::clone(&atom);
             let rv = Arc::clone(&results_vec);
 
             let thread_handle = thread::spawn(move || {
-                let mut rng = rand::thread_rng();
-                let sleep_time_ms: u64 = rng.gen_range(0, 5_000);
-                std::thread::sleep(std::time::Duration::from_millis(sleep_time_ms));
-
-                let new_value = arc.swap(&|x| x + 1);
-
                 rv.swap(&|v: &Vec<usize>| {
                     let mut nv = v.clone();
                     let last = nv.last().unwrap_or_else(|| &0);
                     nv.push(last + 1);
                     nv
                 });
-
-                new_value
             });
 
             thread_handles.push(thread_handle);
         }
 
         for thread_handle in thread_handles {
-            let val = thread_handle.join().unwrap();
-            i_total = i_total + val
-        }
-
-        assert_eq!(results_vec.deref(), (1..51).collect::<Vec<usize>>());
-
-        use std::collections::HashMap;
-        let matom = Arc::new(Atom::new(HashMap::new()));
-
-        let keys = vec!["a", "b", "c", "d"];
-        let mut hm_thread_handles = vec![];
-
-        for k in keys {
-            let arc = Arc::clone(&matom);
-
-            let thread_handle = thread::spawn(move || {
-                let new_value = arc.swap(&|hm: &HashMap<_, _>| {
-                    let mut newhm = hm.clone();
-                    newhm.insert(k.to_string(), "hi".to_string());
-                    newhm
-                });
-
-                new_value
-            });
-
-            hm_thread_handles.push(thread_handle);
-        }
-
-        for thread_handle in hm_thread_handles {
             thread_handle.join().unwrap();
         }
 
-        let mut expected = HashMap::new();
-        expected.insert("a".to_string(), "hi".to_string());
-        expected.insert("b".to_string(), "hi".to_string());
-        expected.insert("c".to_string(), "hi".to_string());
-        expected.insert("d".to_string(), "hi".to_string());
+        assert_eq!(results_vec.deref(), (1..501).collect::<Vec<usize>>());
+    }
+    #[test]
+    fn it_swaps_with_more_expensive_work() {
+        let mut thread_handles = vec![];
 
-        assert_eq!(i_total, 1275); // sum 1-50 inclusive
-        assert_eq!(n, atom.deref());
-        assert_eq!(expected, matom.deref());
+        #[derive(Clone, PartialEq, Debug)]
+        struct Factorial {
+            pub i: u128,
+            pub results: Vec<u128>,
+        }
+
+        let results = Arc::new(Atom::new(Factorial {
+            i: 32,
+            results: vec![],
+        }));
+
+        let n = 32;
+
+        for _ in 0..n {
+            let rv = Arc::clone(&results);
+            let thread_handle = thread::spawn(move || {
+                rv.swap(&|v: &Factorial| {
+                    let mut nv = v.clone();
+                    let ii = v.i;
+                    let new_val = v.results.last().unwrap_or_else(|| &1) * ii;
+                    nv.results.push(new_val);
+                    nv.i = ii - 1;
+                    nv
+                });
+            });
+
+            thread_handles.push(thread_handle);
+        }
+
+        for thread_handle in thread_handles {
+            thread_handle.join().unwrap();
+        }
+
+        let expected = (1..33).rev().fold(vec![], |mut acc_v, x| {
+            let new_val = acc_v.last().unwrap_or_else(|| &1) * x;
+            acc_v.push(new_val);
+            acc_v
+        });
+        assert_eq!(
+            results.deref(),
+            Factorial {
+                i: 0,
+                results: expected.to_vec()
+            }
+        );
     }
 }
